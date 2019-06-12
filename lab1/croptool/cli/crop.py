8819ca2b9d110156b1a5c80cf4fda5da56ce44b6
Andrew Cheung <acheung456@gmail.com>
2019-06-12 17:04:57 -0400

    initial commit

---
+++ lab1/croptool/cli/crop.py
@@ -0,0 +1,31 @@
+import click
+import click_log
+import os
+
+from PIL import Image
+
+@click.group()
+def cli():
+    """Image Cropper"""
+    pass
+
+@cli.command()
+@click.option(
+    "-s", "--source",
+    type=click.Path(exists=True, readable=True)
+)
+@click.option(
+    "-d", "--destination",
+    type=click.Path(exists=True, readable=True)
+)
+def square(source, destination):
+    """Square Crop that image"""
+    src = Image.open(source)
+    cropped = src.resize((min(src.size), min(src.size)))
+    destination = destination or source
+    try:
+        cropped.save(destination)
+    except IOError:
+        print("Unable to save", cropped)
+
+    
---
+++ lab1/croptool/test/test_crop.py
@@ -0,0 +1,20 @@
+import unittest
+import croptool.cli.crop
+
+from click.testing import CliRunner
+from PIL import Image
+
+runner = CliRunner()
+
+class TestCrop(unittest.TestCase):
+    def test_crop(self):
+        raw_image = "../resources/rectangle.png"
+        runner.invoke(croptool.cli.crop.cli(), ['square', '-s', raw_image, '-d', './test_output'])
+        image = Image.open(raw_image)
+
+        new_image = Image.open("./test_output")
+        self.assertEqual(min(image.size), new_image.size[0])
+        self.assertEqual(min(image.size), new_image.size[1])
+
+if __name__ == '__main__':
+    unittest.main()
