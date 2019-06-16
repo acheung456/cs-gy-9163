ba1f82308fab396525314753eed5499f48089df2
Andrew Cheung <acheung456@gmail.com>
2019-06-16 15:10:05 -0400

    Fixing tests and half pixel crop error

--- lab1/croptool/test/test_crop.py
+++ lab1/croptool/test/test_crop.py
@@ -1,21 +1,24 @@
 import unittest
-import croptool.cli.crop
+import os
 
+from croptool.cli import crop
 from click.testing import CliRunner
 from PIL import Image
-
-runner = CliRunner()
 
 
 class TestCrop(unittest.TestCase):
     def test_crop(self):
-        raw_image = "../resources/rectangle.png"
-        runner.invoke(croptool.cli.crop.cli(), ['square', '-s', raw_image, '-d', './test_output'])
+        runner = CliRunner()
+        raw_image = os.path.join(os.path.dirname(__file__), "../resources/longcrop.png")
+        out_image = '/tmp/test_output.png'
+        result = runner.invoke(crop.cli, ['square', '-s', raw_image, '-d', out_image])
         image = Image.open(raw_image)
-
-        new_image = Image.open("./test_output")
-        self.assertEqual(min(image.size), new_image.size[0])
-        self.assertEqual(min(image.size), new_image.size[1])
+        print(result.stdout)
+        new_image = Image.open(out_image)
+        print(new_image.size)
+        x, y = new_image.size[0], new_image.size[1]
+        self.assertEqual(min(image.size), x)
+        self.assertEqual(min(image.size), y)
 
 
 if __name__ == '__main__':
