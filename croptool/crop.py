7b6043af76b4bc0e4ec67c9c8f54bdd38ce8357d
Andrew Cheung <acheung456@gmail.com>
2019-06-17 15:42:17 -0400

    Abstract cli and crop functions

---
+++ croptool/crop.py
@@ -0,0 +1,45 @@
+import math
+import PIL
+
+from enum import Enum
+
+def square(source, destination):
+    """Square crops an image (crops center of image)"""
+    src = PIL.Image.open(source)
+    image_box = ImageBox(src.size)
+    image_box.center_crop()
+    if not all(isinstance(x, int) for x in image_box.box):
+        image_box.transpose()
+    cropped = src.crop(image_box.box)
+    destination = destination or source
+    try:
+        cropped.save(destination)
+    except Exception:
+        print(f"Unable to save image at {destination}")
+
+class Directions(Enum):
+    UP = "up"
+    DOWN = "down"
+
+    @classmethod
+    def has_direction(self, val):
+        return any(val == item.value for item in self)
+
+class ImageBox():
+    def __init__(self, image_size=(0, 0)):
+        self.size = image_size
+        self.box = (0, 0) + image_size
+
+    def center_crop(self):
+        self.box =  (
+            0 if min(self.size) == self.size[0] else (self.size[0]-min(self.size))/2,
+            0 if min(self.size) == self.size[1] else (self.size[1]-min(self.size))/2,
+            min(self.size) if min(self.size) == self.size[0] else self.size[0] - ((self.size[0]-min(self.size))/2),
+            min(self.size) if min(self.size) == self.size[1] else self.size[1] - ((self.size[1]-min(self.size))/2)
+        )
+
+    def transpose(self, direction=Directions.UP):
+        if not Directions.has_direction(direction):
+            raise "Unknown Direction Exception"
+        if direction is Directions.UP:
+            self.box = (math.floor(x) for x in self.box)
